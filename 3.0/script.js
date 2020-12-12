9dd193c866b6b8712fba25065420785a86433fec
riabtsun <riabtsun@i.ua>
2020-12-12 12:23:35 +0200

    add 2b

---
+++ 3.0/script.js
@@ -0,0 +1,60 @@
+console.log(`*****EX-1*****`);
+
+A = [], B = [];
+
+A.length = 10;
+B.length = 10;
+
+for (i = 0, max = 10; i < A.length && i < B.length; i++) {
+	A[i] = Math.floor(Math.random() * Math.floor(max));
+	B[i] = Math.floor(Math.random() * Math.floor(max));
+}
+console.log(A);
+console.log(B);
+
+for (x = 0; x < A.length; x++) {
+	for (y = 0; y < B.length; y++) {
+		if (A[x] == B[y]) {
+			console.log(`${A[x]} = ${B[y]}`);
+			break;
+		}
+	}
+}
+console.log(`*****EX-2*****`);
+arrA = new Array(15);
+arrB = new Array();
+
+for (i = 0, max = 100; i < arrA.length; i++) {
+	arrA[i] = Math.floor(Math.random() * Math.floor(max));
+	prime = true;
+	for (j = 2; j < arrA[i]; j++) {
+		if (arrA[i] % j == 0) {
+			prime = false;
+			break;
+		}
+	}
+	if (prime) {
+		arrB.push(arrA[i]);
+	}
+}
+console.log(arrA);
+console.log(arrB);
+
+console.log(`*****EX-2b*****`);
+
+console.log(arrB);
+
+
+
+for (i = 0, minIndex = 0, maxIndex = 0; i < arrB.length; i++) {
+	if (arrB[i] < arrB[minIndex]) {
+		minIndex = i;
+	}
+	if (arrB[i] > arrB[maxIndex]) {
+		maxIndex = arrB[i]; // не могу понять почему когда я писал maxIndex = i, то цикл сразу останавливается
+	}
+}
+console.log(`min element = ${arrB[minIndex]}, max element = ${maxIndex}`);
+
+
+
