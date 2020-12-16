b3c8b91efb8f5017c7689d2dd6015484834bbab8
riabtsun <riabtsun@i.ua>
2020-12-16 19:28:10 +0200

    my first function

--- 5.0/script.js
+++ 5.0/script.js
@@ -1,8 +1,22 @@
-function calc(firstNum, secondNum) {
-  if (isNaN(firstNum) || isNaN(secondNum)) {
-    alert(`Use only numbers!!!`)
-  } else
-    (console.log(`Результат ${firstNum} + ${secondNum} = ${firstNum + secondNum}`));
+function calc(a, operator, b) {
+  switch (operator) {
+    case '+':
+      console.log(`Результат: ${a} + ${b} = ${a + b}`);
+      break;
+    case '-':
+      console.log(`Результат: ${a} + ${b} = ${a - b}`);
+      break;
+    case '*':
+      console.log(`Результат: ${a} * ${b} = ${a * b}`);
+      break;
+    case '/':
+      if (b == 0) {
+        console.log(`You can't use 0 in this operation`);
+      }
+      else {
+        console.log(`Результат: ${a} / ${b} = ${a / b}`)
+      };
+  }
 }
 
-calc(+prompt(`Enter first number`), +prompt(`Enter second number`));
+calc(+prompt(`Enter first number`), prompt(`Select an operation '+', '-', '*', '/' `), +prompt(`Enter second number`));
