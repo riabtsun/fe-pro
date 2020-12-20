03563295620bccd6a9f1bb6ef0b32864924b8a5a
riabtsun <riabtsun@i.ua>
2020-12-20 21:41:48 +0200

    kvartal

---
+++ table_test/script.js
@@ -0,0 +1,17 @@
+// names = ['Andrew', 'Bohdan', 'Cindy', 'Evan', 'Flake'];
+// surnames = ['Smith', 'Brown', 'Black', 'Roberts', 'Stark'];
+// ages = [20, 18, 35, 16, 29];
+// info = [];
+// info.push(names, surnames, ages);
+
+
+// for (i = 0; i < info.length; i++) {
+//   console.log(info[i]);
+//   newArr = info[i];
+//   base = [];
+//   for (j = 0; j < newArr.length; j++) {
+//     base.push(newArr[j]);
+//     console.log(base);
+//   }
+// }
+
