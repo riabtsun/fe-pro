5988a82464671f33491eb03c41eec8746d89c123
riabtsun <riabtsun@i.ua>
2020-12-23 20:32:28 +0200

    7.1

---
+++ 7.1/script.js
@@ -0,0 +1,21 @@
+function square(num) {
+  return num * num;
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+function main(a, b, c) {
+  let resultSumm = sum(a, b);
+  if (typeof c === 'function') {
+    return c(resultSumm);
+  }
+  return sum;
+}
+
+
+
+main(1, 2, 3);
+
+
