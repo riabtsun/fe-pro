2a7a98e6ba2d706259fa8cc4f850719298dd22fc
riabtsun <riabtsun@i.ua>
2021-01-03 14:36:59 +0200

    lesson 10.1

--- 8.1/script.js
+++ 8.1/script.js
@@ -26,55 +26,28 @@ function hours(value) {
     return value;
   }
 }
-// document.write(`<tr>
-//                   <td>Task name: Write a tutorial</td>
-//                   <td>Task duration: 3 hours</td>
-//                   <td>Task amount: $${price}</td>
-//                 </tr>`)
 
-// let taskName = function tdTask(task) {
-//   return task[0][0] = `<td>Task name: ${task[0][0]}</td>`;
-// }
-let taskDuration = function tdDur(time) {
-  return time[0][1] = `<td>Task duration: ${time[0][1]} hours</td>`;
-}
-let price = function tdPrice(price) {
-  let newtime = price[0][1] * amount;
-  return newtime = `<td>Task amount: ${newtime}</td>`
-}
-// document.write(`<table><tr>${taskName(monday)}${taskDuration(monday)}${price(monday)}</tr></table>`)
-
-// function taskTable(value){
-//   for(value[0] && value[1]){
-
-// }
-// }
 function table(tasks) {
   let html = "";
   for (let i = 0; i < tasks.length; i++) {
     const day = tasks[i];
-
     for (let j = 0; j < day.length; j++) {
       const dayTasks = day[j];
-
       const taskName = dayTasks[0];
       const taskDuration = dayTasks[1];
       const taskAmount = amount * taskDuration;
-
       html += '<tr>';
-      html += `<td>${taskName}</td>`;
-      html += `<td>${taskDuration}</td>`;
-      html += `<td>${taskAmount}</td>`;
+      html += `<td>Task name: ${taskName}</td>`;
+      html += `<td>Taks duration: ${taskDuration} hours</td>`;
+      html += `<td>Task amount: $${taskAmount}</td>`;
       html += '</tr>';
     }
   }
-
   return html;
 }
 
 let tableHtml = '<table>'
-tableHtml += "<tr><th>Name</th><th>Duration</th><th>Amount</th></tr>"
-const tableRows = table(tasks);
+let tableRows = table(tasks);
 tableHtml += tableRows
 tableHtml += '</table>'
 
