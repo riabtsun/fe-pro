24881041dbfb30f3a355ec7f4a9279ea8563dc2e
riabtsun <riabtsun@i.ua>
2021-01-04 19:41:16 +0200

    lesson 10.2

--- 8.2/script.js
+++ 8.2/script.js
@@ -5,68 +5,50 @@ const users = [
   ["Tyson", "green", 1, ["book", "pen"]]
 ];
 
-function getUserNamesWithSign(users, sign) {
-  const newUsersOne =  [];
-  users.forEach(function(user) {
-    const userName = `${user[0]}${sign}`;
 
-    newUsersOne.push(userName)
-  });
-
-  return newUsersOne;
+let usersExclam = [];
+users.forEach(exclam);
+function exclam(user) {
+  let userName = `${user[0]}!`;
+  usersExclam.push(userName);
+  return usersExclam;
 }
+// console.log(usersExclam);
 
-function filterUsers(users, filterValue) {
-  // const filteredUsers = [];
-  // users.forEach(function(user) {
-  //   const filtered = user.filter(function(userItem) {
-  //     return userItem === filterValue;
-  //   });
-  //
-  //   if (filtered.length > 0) {
-  //     filteredUsers.push(user);
-  //   }
-  // });
-  // return filteredUsers;
+let usersQuest = users.map(question);
+function question(user) {
+  return user[0] + `?`;
+}
+// console.log(usersQuest);
 
-  // return users.filter(function(user) {
-  //   const filtered = user.filter(function(userItem) {
-  //     return userItem === filterValue;
-  //   });
-  //   return filtered.length > 0;
-  // });
+let redArr = users.filter(redTeam);
+function redTeam(color) {
+  userColor = color[1];
+  if (userColor === 'red') {
+    return color;
+  }
+}
+// console.log(redArr);
 
-  return users.filter(function(user) {
-    return user[1] === filterValue;
-  });
-};
-
-function getUsersScoreSum(users) {
+function UsersScoreSum(users) {
   let score = 0;
-
   users.forEach(function (user) {
     score += user[2];
   });
-
   return score;
 };
 
-const filteredUsers = filterUsers(users, 'red');
-const finalScore = getUsersScoreSum(filteredUsers);
-
-console.log(getUserNamesWithSign(users, '!'));
-console.log(getUserNamesWithSign(users, '?'));
+const finalScore = UsersScoreSum(redArr);
 
 let tableHtml = '<table>';
 tableHtml += `<thead>`;
 tableHtml += "<tr><th>Name</th><th>Color</th><th>Score</th><th>Items</th></tr>";
 tableHtml += `</thead>`;
-filteredUsers.forEach(function (user) {
+redArr.forEach(function (user) {
   const userName = user[0];
   const userColor = user[1];
   const userScore = user[2];
   const userItems = user[3];
-
   tableHtml += `<tr>`;
   tableHtml += `<td>${userName}</td>`;
   tableHtml += `<td>${userColor}</td>`;
@@ -74,7 +56,7 @@ filteredUsers.forEach(function (user) {
   tableHtml += `<td>${userItems.toString()}</td>`;
   tableHtml += `</tr>`;
 });
-tableHtml += `<tfoot>Final sum ${finalScore}</tfoot>`;
+tableHtml += `<tfoot><tr><td>Final sum = ${finalScore} </td></tr></tfoot>`;
 tableHtml += '</table>';
 
 document.write(tableHtml);
