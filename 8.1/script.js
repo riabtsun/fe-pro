ff667f68353efa4116e853f2d6afb6ffdb3f64ae
riabtsun <riabtsun@i.ua>
2021-01-20 21:57:29 +0200

    lesson 8.1

--- 8.1/script.js
+++ 8.1/script.js
@@ -17,38 +17,73 @@ let tuesday = [
 ];
 let tasks = [monday, tuesday];
 
-let resultMonday = monday.map(hours);
-let resultTuesday = tuesday.map(hours)
+// let resultMonday = monday.map(hours);
+// let resultTuesday = tuesday.map(hours)
 
-function hours(value) {
-  value[1] /= 60;
-  if (value[1] >= 2) {
-    return value;
-  }
-}
+// function hours(value) {
+//   value[1] /= 60;
+//   if (value[1] >= 2) {
+//     return value;
+//   }
+// }
 
-function table(tasks) {
-  let html = "";
-  for (let i = 0; i < tasks.length; i++) {
-    const day = tasks[i];
-    for (let j = 0; j < day.length; j++) {
-      const dayTasks = day[j];
-      const taskName = dayTasks[0];
-      const taskDuration = dayTasks[1];
-      const taskAmount = amount * taskDuration;
-      html += '<tr>';
-      html += `<td>Task name: ${taskName}</td>`;
-      html += `<td>Taks duration: ${taskDuration} hours</td>`;
-      html += `<td>Task amount: $${taskAmount}</td>`;
-      html += '</tr>';
+// function table(tasks) {
+//   let html = "";
+//   for (let i = 0; i < tasks.length; i++) {
+//     const day = tasks[i];
+//     for (let j = 0; j < day.length; j++) {
+//       const dayTasks = day[j];
+//       const taskName = dayTasks[0];
+//       const taskDuration = dayTasks[1];
+//       const taskAmount = amount * taskDuration;
+//       html += '<tr>';
+//       html += `<td>Task name: ${taskName}</td>`;
+//       html += `<td>Taks duration: ${taskDuration} hours</td>`;
+//       html += `<td>Task amount: $${taskAmount}</td>`;
+//       html += '</tr>';
+//     }
+//   }
+//   return html;
+// }
+
+// let tableHtml = '<table>'
+// let tableRows = table(tasks);
+// tableHtml += tableRows
+// tableHtml += '</table>'
+
+// document.write(tableHtml)
+
+tasks = monday.concat(tuesday);
+taskName = ['Task name: ', 'Taks duration:  hours', 'Task amount: $']
+function finalResult(general) {
+  let trs = [];
+  let arr = general.map(function (item) {
+    item[1] /= 60;
+    return trs.push(item)
+  });
+  let hours = arr.filter(function (hours) {
+    if (hours[1] >= 2) {
+      return hours;
     }
-  }
-  return html;
+  });
+  let money = trs.map(function (item) {
+    let price = item[1] * amount;
+    item.push(price);
+    return item;
+  });
+  let rows = money.map(function (all) {
+    tds = all.map(function (td) {
+      td = `<td>${td}</td>`;
+      return td
+    })
+    all = `<tr>${tds.join('')}</tr>`;
+
+    return all
+  });
+
+  document.write(`<table>${rows.join('')}</table>`);
+
+
 }
 
-let tableHtml = '<table>'
-let tableRows = table(tasks);
-tableHtml += tableRows
-tableHtml += '</table>'
-
-document.write(tableHtml)
+finalResult(tasks);
